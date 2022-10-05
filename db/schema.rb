731b6be9012dac4d0c616a1a12843284b1d90558
charlotte divine <divinemaina@gmail.com>
2022-10-05 10:24:32 -0700

    add linters

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,39 +12,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_165955) do
+ActiveRecord::Schema[7.0].define(version: 20_221_005_165_955) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.decimal "price"
-    t.decimal "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'foods', force: :cascade do |t|
+    t.string 'name'
+    t.string 'measurement_unit'
+    t.decimal 'price'
+    t.decimal 'quantity'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "recipe_foods", force: :cascade do |t|
-    t.decimal "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'recipe_foods', force: :cascade do |t|
+    t.decimal 'quantity'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.time "preparation_time"
-    t.time "cooking_time"
-    t.text "description"
-    t.boolean "public"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'recipes', force: :cascade do |t|
+    t.string 'name'
+    t.time 'preparation_time'
+    t.time 'cooking_time'
+    t.text 'description'
+    t.boolean 'public'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
